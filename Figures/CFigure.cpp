fb4928ad99714d865fc251e4f59c84477ef62431
Ali_Said <118924028+Gabaly-8@users.noreply.github.com>
2024-05-06 21:27:04 +0300

    Update CFigure.cpp

--- Figures/CFigure.cpp
+++ Figures/CFigure.cpp
@@ -5,63 +5,6 @@ CFigure::CFigure(GfxInfo FigureGfxInfo)
 { 
 	FigGfxInfo = FigureGfxInfo;	//Default status is non-filled.
 	Selected = false;
-	if (FigGfxInfo.DrawClr == BLACK)
-	{
-		DrawColor = "Black";
-	}
-	else if (FigGfxInfo.DrawClr == BLUE)
-	{
-		DrawColor = "Blue";
-	}
-	else if (FigGfxInfo.DrawClr == GREEN)
-	{
-		DrawColor = "GREEN";
-	}
-	else if (FigGfxInfo.DrawClr == YELLOW)
-	{
-		DrawColor = "Yellow";
-	}
-	else if (FigGfxInfo.DrawClr == RED)
-	{
-		DrawColor = "RED";
-	}
-	else if (FigGfxInfo.DrawClr == ORANGE)
-	{
-		DrawColor = "ORANGE";
-	}
-	else if (FigGfxInfo.DrawClr == NULL)
-	{
-		DrawColor = "NOT FILLED";
-	}
-
-	if (FigGfxInfo.FillClr == BLACK)
-	{
-		FillColor = "Black";
-	}
-	else if (FigGfxInfo.DrawClr == BLUE)
-	{
-		FillColor = "Blue";
-	}
-	else if (FigGfxInfo.DrawClr == GREEN)
-	{
-		FillColor = "GREEN";
-	}
-	else if (FigGfxInfo.DrawClr == YELLOW)
-	{
-		FillColor = "Yellow";
-	}
-	else if (FigGfxInfo.DrawClr == RED)
-	{
-		FillColor = "RED";
-	}
-	else if (FigGfxInfo.DrawClr == ORANGE)
-	{
-		FillColor = "ORANGE";
-	}
-	else if (FigGfxInfo.DrawClr == NULL)
-	{
-		FillColor = "NOT FILLED";
-	}
 }
 
 void CFigure::SetSelected(bool s)
